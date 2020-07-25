6e74fcd24cb49123736014cd588d000ab082205b
Aajinkya Singh <43881544+aajinkya1203@users.noreply.github.com>
2020-07-25 22:59:33 +0530

    Fixed auto-rotate

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'animations.dart';
 import 'pages/home_page.dart';
 import 'utils/themes.dart';
+
 
 class Wrapper extends StatefulWidget {
   @override
@@ -15,6 +17,10 @@ class _WrapperState extends State<Wrapper> {
   Widget build(BuildContext context) {
     final user = Provider.of<FirebaseUser>(context);
     print(user);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: Styles.themeData(context),
       home: user != null ? HomePage() : AuthDemo(),
