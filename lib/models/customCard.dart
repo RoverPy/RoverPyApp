4d7067a53749db338e2a1c0739b90dafce4ba4f4
Aditi-Mohan <54301942+Aditi-Mohan@users.noreply.github.com>
2020-08-21 17:19:13 +0530

    about us page

---
+++ lib/models/customCard.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class CustomCard extends StatefulWidget {
+  final String name;
+  final String image;
+  final String url;
+  final String subtitle;
+  CustomCard({ this.name, this.image, this.url, this.subtitle});
+  @override
+  _CustomCardState createState() => _CustomCardState();
+}
+
+class _CustomCardState extends State<CustomCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: (){},
+        splashColor: Colors.amberAccent,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 12.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(widget.image),
+            ),
+            title: Text(widget.name),
+            trailing: InkWell(
+              onTap: (){
+                launch(widget.url);
+              },
+              child: CircleAvatar(
+                backgroundColor: Colors.black,
+                backgroundImage: AssetImage('assets/github_logo.png'),
+              ),
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/about.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:sign_in/models/customCard.dart';
+import 'package:sign_in/models/developer_info.dart';
+
+import '../models/customCard.dart';
+
+class AboutUs extends StatefulWidget {
+  @override
+  _AboutUsState createState() => _AboutUsState();
+}
+
+class _AboutUsState extends State<AboutUs> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 40.0,),
+          Card(
+            child: ClipPath(
+              clipper: ShapeBorderClipper(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)
+                ),
+              ),
+              child: Container(
+                height: MediaQuery.of(context).size.height/4,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 40.0),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage('https://static.dribbble.com/users/3181935/screenshots/10598987/media/82ab7346bace7f63944f1ef3d2ee3210.jpg'),
+                        fit: BoxFit.cover
+                    )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 20,),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 5.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 3.0,
+                          ),
+                          borderRadius: BorderRadius.circular(20.0)
+                      ),
+                      child: Text(
+                        "About Us",
+                        style: TextStyle(
+                            fontFamily: 'Lexend_Deca',
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35.0
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: developers.length,
+              itemBuilder: (context, index) {
+                return CustomCard(
+                  name: developers[index].name,
+                  image: developers[index].imageLink,
+                  subtitle: developers[index].otherInfo,
+                  url: developers[index].gitLink,
+                );
+              },
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
